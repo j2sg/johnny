b641df308cba0cb3fa87497ac2b834e19e3a6198
Juan Jose Salazar Garcia <jjslzgc@gmail.com>
2015-10-21 15:12:47 +0200

    Modified PasswordSpace class. Moved all code from __str__ to new method alphaPassword and removed needless code

--- passwordspace.py
+++ passwordspace.py
@@ -9,21 +9,21 @@ class PasswordSpace:
         self.currentPassword = 0
         self.maxPassword = int(math.pow(len(self.alphabet), self.length))
 
+
     def __iter__(self):
         return self
+
 
     def next(self):
         if self.currentPassword == self.maxPassword:
             raise StopIteration
         else:
-            result = str(self)
+            result = self.alphaPassword()
             self.currentPassword += 1
             return result
 
-    def __int__(self):
-        return self.currentPassword
 
-    def __str__(self):
+    def alphaPassword(self):
         result = ''
         alphabetLength = len(self.alphabet)
         password = self.currentPassword
@@ -33,3 +33,7 @@ class PasswordSpace:
             password /= alphabetLength
 
         return self.alphabet[password] + result
+
+
+    def __str__(self):
+        return 'Length: {0} characters\nSize: {1} passwords\nAlphabet: {2} characters {3}\n'.format(self.length, self.maxPassword, len(self.alphabet), self.alphabet)
